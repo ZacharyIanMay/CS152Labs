fe1747cc2c97b5027aea6df89de55e0ba0cc618d
ZacharyIanMay <zacharymay12383@gmail.com>
2022-02-18 00:08:10 -0800

    finished Tournament.scala

--- src/main/scala/lab3/DDS.scala
+++ src/main/scala/lab3/DDS.scala
@@ -1,18 +1,21 @@
 package lab3
 
+import scala.annotation.tailrec
+
 
 class DDS:
 
   // problem 1
+  @tailrec
   final def controlLoop[S](state: S, cycle: Int, halt: (S, Int) => Boolean, update: (S, Int) => S): S =
     if(halt(state, cycle)) state
     else controlLoop(update(state, cycle), cycle + 1, halt, update)
 
   // problem 2
-  def h2(x: Int, cycle: Int) = if(x > (10^5)) true else false
-  def u2(x: Int, cycle: Int) = x*2
+  def h2(x: Int, cycle: Int): Boolean = if(x > (10^5)) true else false
+  def u2(x: Int, cycle: Int): Int = x*2
 
-  val population = controlLoop(1, 1, h2, u2)
+  val population: Int = controlLoop(1, 1, h2, u2)
 
   // problem 3
   def solve(f: Double => Double): Double =
@@ -23,31 +26,35 @@ class DDS:
     controlLoop(1.0, 0, goodEnough, improve)
 
   // problem 4
-  def sqrt(x: Double) =
+  def sqrt(x: Double): Double =
     def g(n: Double) = n * n - x
-    solve(g _)
+    solve(g)
 
   // problem 5
-  def cubeRoot(x: Double) =
+  def cubeRoot(x: Double): Double =
     def g(n: Double) = n * n * n - x
-    solve(g _)
+    solve(g)
 
   // problem 6
-  def nthRoot(x: Double, n: Int) =
+  def nthRoot(x: Double, n: Int): Double =
+    @tailrec
     def g(b: Double, v: Double, c: Int): Double =
       if(c == 1) v
       else g(b, b * v, c-1)
     def f(y: Double) = g(y, y, n) - x
-    solve(f _)
+    solve(f)
 
   // problem 7
 
   // = value of an investment of $principle at an anual rate r compounded
   // periods times over 1 year
-  def value(principle: Double, rate: Double, periods: Int): Double = ???
+  def value(principle: Double, rate: Double, periods: Int): Double =
+    def h1(x: Double, cycle: Int): Boolean = if(cycle >= periods) true else false
+    def u1(x: Double, cycle: Int): Double = x * (1+rate/periods)
+    controlLoop(principle, 0, h1, u1)
 
 object TestDDS extends DDS with App:
-  println(nthRoot(4, 2))
+  println(value(50, .1, 12))
   println(nthRoot(64, 3))
   println(nthRoot(16, 4))
   // etc.
--- src/main/scala/lab3/Lab3.scala
+++ src/main/scala/lab3/Lab3.scala
@@ -1,23 +1,26 @@
 package lab3
 
+import scala.annotation.tailrec
+
 class Lab3 {
   // problem 1
   def compose[T, S, V](f: T=>V, g: S=>T): S=>V =
     def ret(x: S) = f(g(x))
-    ret _
+    ret
 
 
   // problem 2
-  def id[T](x: T) = x
+  def id[T](x: T): T = x
   def selfIter[T](f: T => T, n: Int): T => T =
-    if(n == 0) id _
+    if(n == 0) id
     else if(n == 1) f
     else compose(f, selfIter(f, n-1))
 
   // problem 3
   def countPass[T](elems: Array[T], test: T => Boolean): Int =
+    @tailrec
     def helper(c: Int, cur: Int): Int =
-      if(cur == elems.size) c
+      if(cur == elems.length) c
       else if(test(elems(cur))) helper(c + 1, cur + 1)
       else helper(c, cur + 1)
     helper(0, 0)
@@ -29,14 +32,14 @@ class Lab3 {
       var t = baseVal
       for(i <- 1 to n) t = combiner(t, i)
       t
-    ret _
+    ret
 
   // problem 6
   def deOptionize[T, S](f: T => Option[S]): T => S =
     def g(x: T): S =
       val t = f(x)
-      if(t == None) throw new Exception("Invalid Input: " + x)
+      if(t.isEmpty) throw new Exception("Invalid Input: " + x)
       else t.get
-    g _
+    g
 
 }
